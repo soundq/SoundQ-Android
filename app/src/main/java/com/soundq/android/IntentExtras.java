634fee5eba5740cd6fd4d26a28b7fa63fad193bc
ramv13 <ramsai.vellanki@gmail.com>
2016-05-25 20:22:28 -0400

    Added constants for intent extras

---
+++ app/src/main/java/com/soundq/android/IntentExtras.java
@@ -0,0 +1,18 @@
+package com.soundq.android;
+
+/**
+ * Set of constants for intent extras variable names
+ */
+public class IntentExtras {
+
+    /**
+     * Token field from SoundCloud authentication
+     */
+    public static final String TOKEN = "TOKEN";
+
+    /**
+     * Prevents instantiation
+     */
+    private IntentExtras() { }
+
+}
